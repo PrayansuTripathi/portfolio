b2d58844b6968e47c627732229d0484c0f99d3ec
prayansu <prayansutripathi@gmail.com>
2024-01-15 13:48:01 +0530

    add contact data store in google sheet

--- Snippets/footer.js
+++ Snippets/footer.js
@@ -2,7 +2,7 @@ document.getElementById('footer-section').innerHTML = `<footer class="footer">
 <div class="footer__bg">
     <div class="footer__container container grid">
         <div>
-            <h1 class="footer__title">Prayansu</h1>
+            <h1 class="footer__title">Prayansu Tripathi</h1>
             <span class="footer__subtitle">Frontend developer</span>
         </div>
         <ul class="footer__links">
@@ -21,9 +21,11 @@ document.getElementById('footer-section').innerHTML = `<footer class="footer">
         </ul>
         <div class="footer__socials">
             <a href="#" target="_blank" class="footer__social"><i class="uil uil-facebook-f"></i></a>
-            <a href="#" target="_blank" class="footer__social"><i class="uil uil-instagram"></i></a>
-            <a href="#" target="_blank" class="footer__social"><i class="uil uil-twitter-alt"></i></a>
+            <a href="https://twitter.com/PrayansuT97733" target="_blank" class="footer__social"><i class="uil uil-twitter-alt"></i></a>
+            <a href="https://www.linkedin.com/in/prayansu-tripathi/" target="_blank" class="footer__social"><i class="uil uil-linkedin"></i></a>
+            <a href="https://github.com/PrayansuTripathi" target="_blank" class="footer__social"><i class="uil uil-github"></i></a>
         </div>
+        <p>© 2023 All rights reserved by Prayansu Tripathi.</p>
     </div>
 </div>
 </footer>`;
